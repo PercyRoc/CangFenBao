316471ea71f4b9ef215287f98c33406c3e157a29
liupengfei <19531866915@163.com>
2025-07-08 11:11:29 +0800

    优化 AsnOrderSelectionDialogView 类，删除不再使用的命名空间，提升代码可维护性和可读性。

--- XinJuLi/Views/Dialogs/AsnOrderSelectionDialogView.xaml.cs
+++ XinJuLi/Views/Dialogs/AsnOrderSelectionDialogView.xaml.cs
@@ -1,11 +1,9 @@
-using System.Windows.Controls;
-
 namespace XinJuLi.Views.Dialogs
 {
     /// <summary>
     /// AsnOrderSelectionDialogView.xaml 的交互逻辑
     /// </summary>
-    public partial class AsnOrderSelectionDialogView : UserControl
+    public partial class AsnOrderSelectionDialogView
     {
         public AsnOrderSelectionDialogView()
         {
