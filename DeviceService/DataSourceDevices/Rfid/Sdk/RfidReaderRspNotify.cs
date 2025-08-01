c1556d69d5ca94592a5995efd57ef70e58bb4380
liupengfei <19531866915@163.com>
2025-08-01 10:15:09 +0800

    feat: 新增RFID设备管理和通信功能
    - 添加了多个类以支持RFID设备的串口和网络通信，包括RfidReader、RfidTcpServer、Transport等。
    - 实现了异步Socket通信，支持多客户端连接。
    - 增加了RFID参数和工作参数的管理功能。
    - 优化了数据接收和处理逻辑，确保设备的高效通信。

---
+++ DeviceService/DataSourceDevices/Rfid/Sdk/RfidReaderRspNotify.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RfidSdk
+{
+    public interface RfidReaderRspNotify
+    {
+        void OnRecvResetRsp(RfidReader reader,byte result);
+        void OnRecvSetFactorySettingRsp(RfidReader reader, byte result);
+        void OnRecvStartInventoryRsp(RfidReader reader, byte result);
+        void OnRecvStopInventoryRsp(RfidReader reader, byte result);
+        void OnRecvDeviceInfoRsp(RfidReader reader, byte[] firmwareVersion,byte deviceType);
+        void OnRecvSetWorkParamRsp(RfidReader reader, byte result);
+
+        void OnRecvQueryWorkParamRsp(RfidReader reader, byte result, RfidWorkParam workParam);
+
+        void OnRecvSetTransmissionParamRsp(RfidReader reader, byte result);
+
+        void OnRecvQueryTransmissionParamRsp(RfidReader reader, byte result, RfidTransmissionParam transmissiomParam);
+
+        void OnRecvSetAdvanceParamRsp(RfidReader reader, byte result);
+
+        void OnRecvQueryAdvanceParamRsp(RfidReader reader, byte result, RfidAdvanceParam advanceParam);
+
+        void OnRecvTagNotify(RfidReader reader, TlvValueItem[] tlvItems, byte tlvCount);
+
+        void OnRecvHeartBeats(RfidReader reader, TlvValueItem[] tlvItems, byte tlvCount);
+
+        void OnRecvSettingSingleParam(RfidReader reader, byte result);
+
+        void OnRecvQuerySingleParam(RfidReader reader, TlvValueItem item);
+
+        void OnRecvWriteTagRsp(RfidReader reader, byte result);
+
+		void OnRecvRecordNotify(RfidReader reader, String time, String tagId);
+
+        void OnRecvRecordStatusRsp(RfidReader reader, byte result);
+
+        void OnRecvSetRtcTimeStatusRsp(RfidReader reader, byte result);
+
+        void OnRecvQueryRtcTimeRsp(int year, int month, int day, int hour, int min, int sec);
+		
+        void OnRecvReadBlockRsp(RfidReader reader, byte result, byte[] read_data,byte[] epc_data);
+		
+        void OnRecvWriteWiegandNumberRsp(RfidReader reader, byte result);
+
+        void OnRecvLockResult(RfidReader reader, byte result);
+
+        void OnRecvQueryExtParamRsp(RfidReader reader, byte result,RfidExtParam extParam);
+
+        void OnRecvSetExtParam(RfidReader reader, byte result);
+
+        void OnRecvAudioPlayRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvRelayOpRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvVerifyTagRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvSetUsbInfoRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvQueryUsbInfoRsp(RfidSdk.RfidReader reader,byte interfaceType,byte usbProto, byte enterflag);
+
+        void OnRecvSetDataFlagRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvQueryDataFlagRsp(RfidSdk.RfidReader reader, UInt16 dataflag,byte dataformat);
+
+        void OnRecvQueryModbusParam(RfidSdk.RfidReader reader, byte tagNum, byte unionSize, byte startaddr, byte clearflag,int modbusProto);
+
+        void OnRecvSetModbusParamRsp(RfidSdk.RfidReader reader, byte result);
+
+        void OnRecvTagData(RfidReader reader, TlvValueItem[] tlvItems, byte tlvCount);
+    }
+}
